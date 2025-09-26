8068df17a5e3be16cb823cf8c2c4d5d3c086fec8
Mukseda Aktar <muksedamukti@gmail.com>
2025-09-27 01:54:13 +0600

    banner image added

--- src/components/Navbar/Banner/Banner.jsx
+++ src/components/Navbar/Banner/Banner.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
+import bannerImg1 from "../../../assets/vector1.png"
 
 
 const Banner = () => {
     return (
         <div className='max-w-[1440px] mx-auto inter flex justify-between'>
-        <div className='w-[708px] h-[250px] bg-gradient-to-r from-[#632EE3]  to-[#9F62F2] text-white rounded-xl flex flex-col justify-center items-center mb-8 font-bold '>
-            <h1>In-Progress</h1>
+        <div className='w-[708px] h-[250px] bg-gradient-to-r from-[#632EE3]  to-[#9F62F2] text-white rounded-xl flex  justify-between items-center mb-8 font-bold '>
+            <div>
+                <img  src={bannerImg1} alt="" />
+            </div>
+            <div>
+                <h1>In-Progress</h1>
             <p className='font-semibold text-[60px]'><span>0</span></p>
+            </div>
+            <div>
+                <img className='rotate-180 scale-y-[-1]' src={bannerImg1} alt="" />
+            </div>
+            
 
         </div>
-        <div className='w-[708px] h-[250px] bg-gradient-to-r from-[#54CF68]  to-[#00827A] text-white rounded-xl flex flex-col justify-center items-center font-bold mb-8'>
-            <h1>Resolved</h1>
+        <div className='w-[708px] h-[250px] bg-gradient-to-r from-[#54CF68]  to-[#00827A] text-white rounded-xl flex  justify-between items-center font-bold mb-8'>
+            <div>
+                <img  src={bannerImg1} alt="" />
+            </div>
+            <div>
+                <h1>Resolved</h1>
             <p className='font-semibold text-[60px]'><span>0</span></p>
-
+            </div>
+            <div>
+                <img className='rotate-180 scale-y-[-1]' src={bannerImg1} alt="" />
+            </div>
+            
         </div>
         
         </div>
